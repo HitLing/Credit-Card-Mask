6bd7eaf576617fcc25ca332270034a1c2d623c12
HitLing <31694201+HitLing@users.noreply.github.com>
2021-03-23 11:19:45 +0300

    Add files via upload

---
+++ Маска кредитной карты ката 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public static class Kata
+{
+    // return masked string
+    public static string Maskify(string cc)
+    {
+        int length = cc.Length;
+        if (length>4) {
+            char[] str = cc.ToCharArray();
+            for (int i = 0; i < length-4; i++) {
+                str[i] = Convert.ToChar("#");
+            }
+            cc = new string(str);
+        }
+
+        return cc;
+    }
+
+}
+
+
+
+
+namespace Маска_кредитной_карты_ката_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string test = Kata.Maskify("");
+            Console.WriteLine(test);
+        }
+    }
+}
